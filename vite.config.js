4eaa1adf9986df7d3a0c65b589e691d4769cf2ce
riddhima <riddhima@riddhimas-MacBook-Air.local>
2025-05-29 14:29:30 +0530

    Fix: rebuild and include images

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/minervasociety/',  // 👈 this is critical for GitHub Pages
   plugins: [react()],
 })
